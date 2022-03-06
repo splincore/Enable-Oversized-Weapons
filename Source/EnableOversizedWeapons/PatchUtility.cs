a0a2a60ef67d9a6fad30891c2f5a21c965d226c9
Carny Senpai <splincore@web.de>
2022-03-06 18:59:08 +0100

    updated for weapon styles

--- Source/EnableOversizedWeapons/PatchUtility.cs
+++ Source/EnableOversizedWeapons/PatchUtility.cs
@@ -7,25 +7,25 @@ namespace EnableOversizedWeapons
     {
         public static Vector3 GetDrawOffset(Vector3 drawLoc, Thing thing, Pawn pawn)
         {
-            return drawLoc + thing.def.graphicData.DrawOffsetForRot(pawn.Rotation);
+            return drawLoc + thing.Graphic.DrawOffset(pawn.Rotation);
         }
 
         public static Mesh GetMesh(Mesh mesh, Thing thing, float aimAngle)
         {
-            if (thing.def.graphicData.drawSize == Vector2.one) return mesh;
+            if (thing.Graphic.drawSize == Vector2.one) return mesh;
             if (aimAngle > 200f && aimAngle < 340f)
             {
-                return MeshPool.GridPlaneFlip(thing.def.graphicData.drawSize);
+                return MeshPool.GridPlaneFlip(thing.Graphic.drawSize);
             }
             else
             {
-                return MeshPool.GridPlane(thing.def.graphicData.drawSize);
+                return MeshPool.GridPlane(thing.Graphic.drawSize);
             }
         }
 
         public static Vector3 RemoveNorthDrawOffsetFromEquipment(Vector3 drawLoc, Thing thing)
         {
-            if (thing.Rotation == Rot4.North && thing.def.equipmentType == EquipmentType.Primary) drawLoc -= thing.def.graphicData.DrawOffsetForRot(Rot4.North);
+            if (thing.Rotation == Rot4.North && thing.def.equipmentType == EquipmentType.Primary) drawLoc -= thing.Graphic.DrawOffset(Rot4.North);
             return drawLoc;
         }
     }
