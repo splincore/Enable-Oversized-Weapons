4ec2bc8c771d2f1dd53779be227df2875d79c075
Carny Senpai <splincore@web.de>
2022-03-22 21:22:36 +0100

    bugfix styles, test for private mesh dictionary

--- Source/EnableOversizedWeapons/PatchUtility.cs
+++ Source/EnableOversizedWeapons/PatchUtility.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using Verse;
 
 namespace EnableOversizedWeapons
@@ -7,7 +8,14 @@ namespace EnableOversizedWeapons
     {
         public static Vector3 GetDrawOffset(Vector3 drawLoc, Thing thing, Pawn pawn)
         {
-            return drawLoc + thing.Graphic.DrawOffset(pawn.Rotation);
+            if (thing.StyleDef != null && thing.StyleDef.graphicData != null)
+            {
+                return drawLoc + thing.StyleDef.graphicData.DrawOffsetForRot(pawn.Rotation);
+            }
+            else
+            {
+                return drawLoc + thing.def.graphicData.DrawOffsetForRot(pawn.Rotation);
+            }
         }
 
         public static Mesh GetMesh(Mesh mesh, Thing thing, float aimAngle)
@@ -15,11 +23,11 @@ namespace EnableOversizedWeapons
             if (thing.Graphic.drawSize == Vector2.one) return mesh;
             if (aimAngle > 200f && aimAngle < 340f)
             {
-                return MeshPool.GridPlaneFlip(thing.Graphic.drawSize);
+                return GridPlaneFlip(thing.Graphic.drawSize); // Test bugfix
             }
             else
             {
-                return MeshPool.GridPlane(thing.Graphic.drawSize);
+                return GridPlane(thing.Graphic.drawSize); // Test bugfix
             }
         }
 
@@ -28,5 +36,29 @@ namespace EnableOversizedWeapons
             if (thing.Rotation == Rot4.North && thing.def.equipmentType == EquipmentType.Primary) drawLoc -= thing.Graphic.DrawOffset(Rot4.North);
             return drawLoc;
         }
+
+        // Test with own dictionary for pawn draw size bug
+        private static Mesh GridPlane(Vector2 size)
+        {
+            if (!planes.TryGetValue(size, out Mesh mesh))
+            {
+                mesh = MeshMakerPlanes.NewPlaneMesh(size, false, false, false);
+                planes.Add(size, mesh);
+            }
+            return mesh;
+        }
+
+        private static Mesh GridPlaneFlip(Vector2 size)
+        {
+            if (!planesFlip.TryGetValue(size, out Mesh mesh))
+            {
+                mesh = MeshMakerPlanes.NewPlaneMesh(size, true, false, false);
+                planesFlip.Add(size, mesh);
+            }
+            return mesh;
+        }
+
+        private static Dictionary<Vector2, Mesh> planes = new Dictionary<Vector2, Mesh>(FastVector2Comparer.Instance);
+        private static Dictionary<Vector2, Mesh> planesFlip = new Dictionary<Vector2, Mesh>(FastVector2Comparer.Instance);
     }
 }
